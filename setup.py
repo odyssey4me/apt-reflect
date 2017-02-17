110aa19b92fa62d264fd43c48809d5fef755803d
Sam Yaple <sam@yaple.net>
2017-02-17 07:32:48 +0000

    Change script name
    Add entrypoint
    Add requirements.txt

--- setup.py
+++ setup.py
@@ -1,13 +1,18 @@
 from distutils.core import setup
 setup(
-  name = 'apt-reflect',
-  packages = ['apt-reflect'],
-  version = '0.1.2',
-  description = 'APT mirror to object storage',
-  author = 'Sam Yaple',
-  author_email = 'sam+apt-reflect@yaple.net',
-  url = 'https://github.com/SamYaple/apt-reflect',
-  download_url = 'https://github.com/SamYaple/apt-reflect/tarball/0.1.2',
-  keywords = ['radosgw', 'swift', 's3', 'rgw', 'apt', 'deb', 'mirror'],
-  classifiers = [],
+    name = 'apt-reflect',
+    packages = ['apt_reflect'],
+    version = '0.1.2',
+    description = 'APT mirror to object storage',
+    author = 'Sam Yaple',
+    author_email = 'sam+apt-reflect@yaple.net',
+    url = 'https://github.com/SamYaple/apt-reflect',
+    download_url = 'https://github.com/SamYaple/apt-reflect/tarball/0.1.2',
+    keywords = ['radosgw', 'swift', 's3', 'rgw', 'apt', 'deb', 'mirror'],
+    classifiers = [],
+    entry_points={
+        'console_scripts': [
+            'apt-reflect=apt_reflect.apt_reflect:main',
+        ],
+    },
 )
