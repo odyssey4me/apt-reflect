1d7e299a19adef129ec2da1f39fb42d044a9ca13
Sam Yaple <sam@yaple.net>
2017-02-20 15:15:41 +0000

    Refactor sources indices
    AKA Who says long functions are bad

--- apt_reflect/indices/sources.py
+++ apt_reflect/indices/sources.py
@@ -13,56 +13,64 @@ OPT_MAP = {
 
 class SourcesIndex:
     def __init__(self, data):
-        self.files = dict()
-        self._parse(data)
-
-    def _parse(self, data):
-        def save(info):
-            if not info:
-                return
-            directory = info.pop('Directory')
-            for k, v in info.items():
-                path = '/'.join([directory, k])
-                self.files[path] = v
-
-        word_opt = set([
+        self.word_opt = set([
             'Directory',
         ])
-        multiline_opt = set([
+        self.multiline_opt = set([
             'Checksums-Sha1',
             'Checksums-Sha256',
             'Checksums-Sha512',
             'Files',
         ])
 
+        self.files = dict()
+        self._parse(data)
+
+    def _parse(self, data):
         info = dict()
+        section = str()
         for line in data.split('\n'):
             if not line.strip():
-                save(info)
+                self._save_info(info)
                 info = dict()
                 continue
 
-            if re.match(r'\s', line):
-                if not section:
-                    continue
-                checksum, size, filename = line.split()
-                size = int(size)
-
-                if filename not in info:
-                    info[filename] = dict()
-                if 'size' in info[filename] and size != info[filename]['size']:
-                    LOG.error('size mismatch for file: {}'.format(filename))
-                else:
-                    info[filename]['size'] = size
-                info[filename][section] = checksum
+            if not re.match(r'\s', line):
+                section = self._parse_line(line, info)
             else:
-                section = None
-                split = line.split(':', 1)
-                opt = split[0].strip()
-                value = split[-1].strip()
+                self._parse_multiline(line, info, section)
+        else:
+            self._save_info(info)
 
-                if opt in word_opt:
-                    info[opt] = value
-                elif opt in multiline_opt:
-                    section = OPT_MAP[opt]
-        save(info)
+    def _save_info(self, info):
+        if not info:
+            return
+        directory = info.pop('Directory')
+        for k, v in info.items():
+            path = '/'.join([directory, k])
+            self.files[path] = v
+
+    def _parse_line(self, line, info):
+        split = line.split(':', 1)
+        opt = split[0].strip()
+        value = split[-1].strip()
+
+        if opt in self.word_opt:
+            info[opt] = value
+        elif opt in self.multiline_opt:
+           return OPT_MAP[opt]
+
+    def _parse_multiline(self, line, info, section):
+        if not section in ['md5', 'sha1', 'sha256', 'sha512']:
+            return
+
+        checksum, size, filename = line.split()
+        size = int(size)
+        if filename not in info:
+            info[filename] = dict()
+        if 'size' in info[filename] and size != info[filename]['size']:
+            LOG.error('size mismatch for file: {}'.format(filename))
+            raise
+        else:
+            info[filename]['size'] = size
+        info[filename][section] = checksum
