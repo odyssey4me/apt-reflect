3a0117e60508630c01f474ba3b3d961cea50fdb6
Sam Yaple <sam@yaple.net>
2017-02-25 01:28:39 +0000

    Update the purge script
    Do a single request with 1000 objects rather than a delete per object

--- apt_reflect/purge.py
+++ apt_reflect/purge.py
@@ -5,28 +5,22 @@ import threading
 
 import boto3
 
+
 def main():
-    threads = 20
-    q = queue.Queue()
-    for i in range(threads):
-        t = threading.Thread(target=do_work, args=(q,))
-        t.daemon = True
-        t.start()
+    session = boto3.session.Session()
+    s3 = session.resource('s3', endpoint_url='http://10.10.1.1:7480')
+    bucket = s3.Bucket('testing')
+    items = set([x.key for x in bucket.objects.all()])
+    delete = {'Objects': []}
+    for i in bucket.objects.all():
+        if len(delete) < 1000:
+            delete['Objects'].append({'Key': i.key})
+        else:
+            bucket.delete_objects(Delete=delete)
+            delete['Objects'] = [{'Key': i.key}]
+    else:
+        bucket.delete_objects(Delete=delete)
 
-    s3_client = boto3.client('s3', endpoint_url='http://10.10.1.1:7480')
-    while s3_client.list_objects(Bucket='testing')['Contents']:
-        for i in s3_client.list_objects(Bucket='testing')['Contents']:
-            q.put(i['Key'])
-        q.join()
-        print('done loop')
-
-def do_work(work_queue):
-    while True:
-        item = work_queue.get()
-        s3_client = boto3.client('s3', endpoint_url='http://10.10.1.1:7480')
-        s3_client.delete_object(Key=item, Bucket='testing')
-        work_queue.task_done()
-        print(item)
 
 if __name__ == '__main__':
     main()
